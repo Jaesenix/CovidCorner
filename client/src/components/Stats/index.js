4b830e1b2174bb4cdb44e6befc5fd5315eddc3bc
Jacqueline Dean <jacquelineadean@gmail.com>
2020-08-24 18:03:11 -0500

    Debugging

---
+++ client/src/components/Stats/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import API from "../../utils/API"
+
+class Stats extends React.Component {
+    render() {
+        return (
+            <section>
+                
+            </section>
+        )
+    }
+}
+
+export default Stats;
