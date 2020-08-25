2c810adad80a0a6403d1f82e2bcab1c6da44e1eb
Jacqueline Dean <jacquelineadean@gmail.com>
2020-08-25 10:32:38 -0500

    Automating scripts

--- client/src/components/Stats/index.js
+++ client/src/components/Stats/index.js
@@ -2,10 +2,37 @@ import React from "react";
 import API from "../../utils/API"
 
 class Stats extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {total: ""}
+    }
+
+    componentDidMount() {
+        this.addTotal();
+    }
+
+    addTotal = () => {
+        API.getData().then(res => {
+            const positiveArray = res.data.map(statePositive => (
+                statePositive.positive
+            ))
+            const reducer = (accumulator, currentValue) => accumulator + currentValue;
+            const totalPostive = positiveArray.reduce(reducer);
+            this.setState({total: totalPostive});
+            console.log(totalPostive);
+        })
+    }
+
     render() {
         return (
             <section>
-                
+                <div>
+                    <h1>Cases and Deaths in the US</h1>
+                    <div>
+                        <h4>Total Cases</h4>
+                        <h3>{total}</h3>
+                    </div>
+                </div>
             </section>
         )
     }
