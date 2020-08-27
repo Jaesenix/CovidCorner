d6786de83009806a3ffc52ed9c405bd274c01a20
Jacqueline Dean <jacquelineadean@gmail.com>
2020-08-27 11:14:44 -0500

    Backend user sign up data

--- models/user.js
+++ models/user.js
@@ -16,6 +16,16 @@ module.exports = function(sequelize, DataTypes) {
     password: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+    // Booleans for user responses
+    mask: {
+      type: DataTypes.BOOLEAN
+    },
+    unemployed: {
+      type: DataTypes.BOOLEAN
+    },
+    household: {
+      type: DataTypes.BOOLEAN
     }
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
