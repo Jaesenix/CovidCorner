ed09a37c4db488f878c64f8c3308130568372e75
Jessamine Brown <brownjessa11@gmail.com>
2020-08-27 12:19:51 -0500

    fix this file for jessbranch

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,30 @@
-import React from 'react';	import React from "react";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'	import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-import './App.css';	
-
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "./App.css";
-import "fontsource-roboto";
 
+import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
+import LiveUpdates from "./pages/LiveUpdates";
+import SignUp from "./pages/Signup";
+import Members from "./pages/Members";
+import Home from "./pages/Home";
 
-import Login from "./pages/Login";	import Login from "./pages/Login";
-import Navbar from "./components/Navbar";	import Navbar from "./components/Navbar";
-@@ -11,24 +11,21 @@ import SignUp from "./pages/Signup";
-import Members from "./pages/Members";	import Members from "./pages/Members";
-import Home from "./pages/Home";	import Home from "./pages/Home";
-
-
-
-function App() {	function App() {
-
+function App() {
   return (
-    return (	    <Router>
-<Router>	      <div>
-    <div>	        <Navbar />
-    <Navbar />	        <Switch>
-    <Switch>	          <Route exact path="/" component={Home} />
-    <Route exact path="/" component={Home} />	
-          <Route exact path="/login" component={Login} />	          <Route exact path="/login" component={Login} />
-          <Route exact path="/liveupdates" component={LiveUpdates} />	          <Route exact path="/liveupdates" component={LiveUpdates} />
-          <Route exact path="/signup" component={SignUp} />	          <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/members" component={Members} />	          <Route exact path="/members" component={Members} />
-    </Switch>	        </Switch>
-
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/liveupdates" component={LiveUpdates} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/members" component={Members} />
+        </Switch>
       </div>
-
     </Router>
-    </div>	  );
- </Router>	}
-    )}	
-
+  );
+}
 
 export default App;
