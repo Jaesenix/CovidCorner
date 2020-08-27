256265ee192ad624a434f9ea0f966024506366fa
Janella Senica <janella.senica@gmail.com>
2020-08-27 04:12:39 -0500

    added mapbox component

--- client/src/pages/LiveUpdates.js
+++ client/src/pages/LiveUpdates.js
@@ -1,11 +1,13 @@
 import React from "react";
 import NYT from "../components/NYTimes";
+import Mapbox from "../components/Mapbox";
 
 function LiveUpdates () {
     return (
        <div>
            Live Updates
            <NYT></NYT>
+           <Mapbox></Mapbox>
        </div>
     )
 }
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,12 +2,12 @@ import axios from 'axios';
 
 export default {
     // This function is to check if the user is logged in, and to return their info if they are not
-    getLocation: () => {
-            return axios.get("/api/geolocation")
-        },
+    getGeoMap: () => {
+        return axios.get("/api/geolocation")
+    },
     getArticles: () => {
-            return axios.get("/api/articles")
-        },
+        return axios.get("/api/articles")
+    },
     checkUserInfo: () => {
         return axios.get("/api/user_data");
     },
