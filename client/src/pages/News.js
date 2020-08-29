a5c0d0edbe1e059fbc07634751411069aedf75f4
Janella Senica <janella.senica@gmail.com>
2020-08-29 16:58:39 -0500

    rev TestCtrs

--- client/src/pages/News.js
+++ client/src/pages/News.js
@@ -1,16 +1,12 @@
 import React from "react";
 import NYT from "../components/NYTimes";
-import Mapbox from "../components/Mapbox";
+import TestCtrs from "../components/TestCtrs";
 
 function News () {
     return (
        <div>
-<<<<<<< HEAD:client/src/pages/LiveUpdates.js
-           <NYT></NYT>
-           <Mapbox></Mapbox>
-=======
+           <TestCtrs />
            <NYT />
->>>>>>> a8688721fe779b497c294e6b24e90c0c22e8e7b2:client/src/pages/News.js
        </div>
     )
 }
